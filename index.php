4d78976ffbd722532b5a69b0e1f017b51e41d905
Рома <kibroma@mail.ru>
2013-06-13 13:02:30 +0300

    0.01

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+</head>
+<body>
+
+  <div id="header">My GitHub</div>
+  <div id="main">
+	
+	<div id="leftcolumn">
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	</div>
+	<div id="maincolumn">
+	
+	
+	
+	</div>
+	<div id="rightcolumn"></div>
+	
+  </div>
+  <div id="footer">My GitHub</div>
+
+</body>
+
+</html>
