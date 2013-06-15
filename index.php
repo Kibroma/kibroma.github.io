fcaac98ca0b74c6b16f74ef2f9c97bb24a4d7254
Рома <kibroma@mail.ru>
2013-06-15 11:34:38 +0300

    0.02

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+</head>
+<body>
+
+  <div id="header"></div>
+  <div id="main">
+	<div id="leftcolumn">
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	  <ul id="course">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	  </ul>
+	</div>
+	<div id="rightcolumn">
+	
+		
+	
+	</div>
+	<div id="maincolumn">
+	
+	
+	
+	</div>
+	
+  </div>
+  <div id="footer">My GitHub</div>
+
+</body>
+
+</html>
